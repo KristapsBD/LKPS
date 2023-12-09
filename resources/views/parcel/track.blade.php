4ff181109c95fb0518068cca740543bc383dc561
Kristaps Briks <kristaps.briks@inbox.lv>
2023-12-09 21:56:06 +0200

    Set up tracking page

--- resources/views/parcel/track.blade.php
+++ resources/views/parcel/track.blade.php
@@ -8,18 +8,32 @@
 
 @section('content')
     <div class="max-w-md mx-auto my-8">
-        <form id="track-form" action="{{ route('parcel.track') }}" method="POST" class="max-w-sm mx-auto">
+        <form id="track-form" action="{{ route('parcel.track') }}" method="POST" class="max-w-md mx-auto">
             @csrf
             <div class="mb-5">
                 <label for="tracking_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tracking Code</label>
                 <input type="text" id="tracking_code" name="tracking_code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Tracking Code" required>
                 <p id="validation-error" class="text-red-600 text-xs hidden"></p>
             </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Track Parcel</button>
+            <button type="submit" class="text-white grow-0 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Track Parcel</button>
         </form>
 
-        <div id="tracking-info" class="mt-4">
-            <!-- Tracking information will be displayed here dynamically -->
+        <div id="tracking-card" class="hidden mt-4 max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="{{ asset('/images/courier_tracking.jpg')  }}" alt="Tracking page photo" />
+            </a>
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your Parcel Tracking Information:</h5>
+                </a>
+                <div id="tracking-info" class="mt-4">
+                    <!-- Tracking information will be displayed here dynamically -->
+                </div>
+            </div>
+        </div>
+
+        <div id="tracking-error" class="mt-4 hidden">
+            <!-- Tracking error will be displayed here dynamically -->
         </div>
     </div>
 
@@ -27,11 +41,11 @@
         // Use Ajax to fetch tracking information
         $('#track-form').submit(function (e) {
             e.preventDefault();
-            $('#validation-error').addClass('hidden').text();
+            $('#validation-error').addClass('hidden');
+            $('#tracking-error').addClass('hidden');
 
             var trackingCode = $('#tracking_code').val();
 
-            // Use jQuery Ajax to send a request to the server
             $.ajax({
                 url: '{{ route("parcel.track") }}',
                 type: 'POST',
@@ -42,20 +56,24 @@
                 data: JSON.stringify({ tracking_code: trackingCode }),
                 dataType: 'json',
                 success: function (data) {
-                    // Update the content dynamically based on the received data
+                    $('#tracking-card').removeClass('hidden');
                     $('#tracking-info').html(`
-                        <h3 class="text-lg font-semibold dark:text-white">Tracking Information</h3>
                         <p class="font-semibold dark:text-white">Status: ${data.status}</p>
                     `);
                 },
                 error: function (error) {
                     console.error('Error:', error);
-                    $('#validation-error').removeClass('hidden').text(error.responseJSON.message);
-                    if (error.status === 404) {
-                        $('#tracking-info').html('<p class="dark:text-white">' + error.responseJSON.error + '</p>');
 
+                    $('#tracking-card').addClass('hidden');
+                    if (error.status === 422) {
+                        $('#validation-error').removeClass('hidden').text(error.responseJSON.message);
+                    }
+                    else if (error.status === 404) {
+                        $('#tracking-error').removeClass('hidden');
+                        $('#tracking-error').html('<p class="dark:text-white">' + error.responseJSON.error + '</p>');
                     } else {
-                        $('#tracking-info').html('<p class="dark:text-white">Error retrieving tracking information.</p>');
+                        $('#tracking-error').removeClass('hidden');
+                        $('#tracking-error').html('<p class="dark:text-white">Error retrieving tracking information.</p>');
                     }
                 }
             });
